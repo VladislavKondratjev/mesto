699ba50f45464856bc9fd9f6a8692a7f106f9937
VladislavKondratjev <kondruat-kondruatiev@yandex.ru>
2020-11-09 20:49:04 +0300

    feature/template cards

--- scripts/index.js
+++ scripts/index.js
@@ -36,5 +36,47 @@ form.addEventListener('submit', submitForm);
 editButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
+//спринт 5
+//добавляем элементы в темплейт
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const elements = document.querySelector('.elements');
+
+function createCard(data) {
+    const elementTemplate = document.querySelector('.element-template').content;
+    const elementCard = elementTemplate.cloneNode(true);
+
+    elementCard.querySelector('.element__photo').src = data.link;
+    elementCard.querySelector('.element__place').textContent = data.name;
+
+    elements.append(elementCard);
+}
+
+initialCards.forEach(createCard);
 
   
